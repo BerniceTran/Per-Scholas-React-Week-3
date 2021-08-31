e9023010a43eddfe0c9e0ce962c8c8f1b94c74b3
Bernice Tran <bernicetran94@gmail.com>
2021-08-31 01:46:08 -0400

    Add react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+//Hello component used inside App component
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
 }
+
+//React component with name App
+const App = () => {
+  console.log('Hello from component');
+
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+  <div>
+    <h1>Greetings</h1>
+    <Hello name="Bernice" age="26"/>
+    <Hello name={name} age={age}/>
+  </div>
+  )
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
